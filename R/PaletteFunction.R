b848b89d18a2faa38e74631bb39e07e049989874
Amy R. Sweeny <amyr.sweeny@gmail.com>
2021-03-25 16:11:25 +0000

    bug fixed for real??

--- R/PaletteFunction.R
+++ R/PaletteFunction.R
@@ -1,9 +1,46 @@
-QualBlooms <- readRDS('Palettes/QualBlooms.rds')
-SeqBlooms <- readRDS('Palettes/SeqBlooms.rds')
-DivBlooms <- readRDS('Palettes/DivBlooms.rds')
+#QualBlooms <- readRDS('Palettes/QualBlooms.rds')
+#SeqBlooms <- readRDS('Palettes/SeqBlooms.rds')
+#DivBlooms <- readRDS('Palettes/DivBlooms.rds')
 
 
+#' Complete list of palettes
 #'
+#'#' Use \code{\link{bloom_palette}} to construct palettes of desired length.
+#'
+#' @export
+QualBlooms <- list(
+  Episode1 = c("#9FA386","#A3765A", "#AC2031", "#A3504F", "#3EA21B", "#A99F84"),
+  Episode2 = c("#61B0A6", "#BBB8C0", "#BF866D", "#C57284", "#B3BAA5", "#0854B1"),
+  Episode3 = c("#D09C3D", "#43A7BA", "#C8918C", "#C1657C", "#C99776", "#9DBE96"),
+  Episode4 = c("#E5BDDA", "#E3B498", "#E98C97", "#E5B3A9", "#EEA0C2", "#B2E4E1"),
+  Episode5 = c("#B3A341", "#AC518A", "#C56259","#C25B69", "#BE855B", "#6088AB"),
+  Episode6 = c("#B1A6C7", "#9F9B5C", "#A86D77", "#A7685C", "#B39CAF", "#3D4E8F"),
+  Episode7 = c("#BEAC3A", "#DB8A6D", "#EA5065", "#E36058", "#C8B3C9", "#C8906B"),
+  Finale = c("#A28FB1", "#C9BB3F", "#B95D68", "#BD7B6C", "#CF80B8", "#55B5B6")
+)
+
+SeqBlooms <- list(
+  Episode1 = c('#A1665F','#A1705D','#A28065','#A49C81','#839C56','#51A310','#819976','#A92536'),
+  Episode2 = c('#447719','#077B7D','#0B647D','#05375B','#032E65','#023084','#031D72','#64196D'),
+  Episode3 = c('#9F503E','#834F27','#AE7D2D','#2B711F','#09596B','#0F3263','#692755','#A32843'),
+  Episode4 = c('#A95E58','#BB6F5F','#B5745D','#C58F6F','#C49E72','#747870','#85528F','#E783A5'),
+  Episode5 = c('#B03B2D','#9E522F','#98732E','#356E27','#174957','#442E59','#802953','#972838'),
+  Episode6 = c('#A06460','#8F5342','#985B3A','#A0774C','#787C54','#535E8F','#9F8A9F','#9A656F'),
+  Episode7 = c('#D75048','#CD5F4A','#A85C40','#815431','#957730','#88832B','#27472F','#E84F73'),
+  Finale = c('#43790F','#013D27','#024644','#022F3F','#021F32','#031835','#2D0A42','#7E1152')
+)
+
+DivBlooms <- list(
+  Episode1 = c('#C7071D','#D54555','#E3838E','#F1C1C6','#FFFFFF','#EAF0D0','#D4E2A2','#C0D373','#ABC545'),
+  Episode2 = c('#D58056','#DF9F80','#EABFAA','#F4DFD4','#FFFFFF','#C6C3F2','#8E87E6','#564BDA','#1E0FCE'),
+  Episode3 = c('#E35E8B','#EA86A8','#F1AEC4','#F8D6E2','#FFFFFF','#F8E6D5','#F1CEAB','#EAB681','#E39E57'),
+  Episode4 = c('#F594B9','#F7AECA','#FAC9DC','#FCE4ED','#FFFFFF','#FCE8E1','#F9D1C3','#F5BAA5','#F3A388'),
+  Episode5 = c('#E94A59','#EE7782','#F3A4AC','#F9D1D5','#FFFFFF','#F9DCD5','#F3B9AB','#EE9581','#E97358'),
+  Episode6 = c('#E16779','#E88D9A','#F0B3BC','#F7D9DD','#FFFFFF','#F7E2DB','#F0C6B7','#E8AA93','#E18E70'),
+  Episode7 = c('#FF5768','#FF818D','#FFABB3','#FFD5D9','#FFFFFF','#FFDCD6','#FFBAAE','#FF9786','#FF755E'),
+  Finale = c('#E85DA1','#ED85B8','#F3AED0','#F9D6E7','#FFFFFF','#F8F2D8','#F2E5B1','#ECD88A','#E6CC63')
+)
+
 #'
 #' palette generator
 #'
